aafff58b2b54c8659300e7ec9fb5c447fccb3502
aten88 <virtuoso_ten@mail.ru>
2024-01-24 12:51:01 +0300

    Сделал проверку уникальности поля name

--- app/api/charity_project.py
+++ app/api/charity_project.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from app.crud.charity_project import create_charity_project
+from app.crud.charity_project import create_charity_project, get_project_id_by_name
 from app.schemas.charity_project import CharityProjectCreate
 
 router = APIRouter()
@@ -10,5 +10,11 @@ router = APIRouter()
 async def create_new_charity_project(
     charity_project: CharityProjectCreate,
 ):
+    project_id = await get_project_id_by_name(charity_project.name)
+    if project_id is not None:
+        raise HTTPException(
+            status_code=422,
+            detail='Проект с таким именем уже существует!',
+        )
     new_project = await create_charity_project(charity_project)
     return new_project
--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,4 +1,7 @@
+from typing import Optional
 from datetime import datetime, timezone
+
+from sqlalchemy import select
 from app.core.db import AsyncSessionLocal
 from app.models.charity_project import CharityProject
 from app.schemas.charity_project import CharityProjectCreate
@@ -9,11 +12,22 @@ async def create_charity_project(
 ) -> CharityProject:
     """ Метод-корутина для создания обьекта CharityProject. """
     new_project_data = new_project.dict()
-    if new_project_data['create_date'] is None:
-        new_project_data['create_date'] = datetime.now(timezone.utc)
+    new_project_data['create_date'] = datetime.now(timezone.utc)
     db_project = CharityProject(**new_project_data)
     async with AsyncSessionLocal() as session:
         session.add(db_project)
         await session.commit()
         await session.refresh(db_project)
     return db_project
+
+
+async def get_project_id_by_name(project_name: str) -> Optional[int]:
+    """ Метод-корутина для поиска id обьекта по имени. """
+    async with AsyncSessionLocal() as session:
+        db_project_id = await session.execute(
+            select(CharityProject.id).where(
+                CharityProject.name == project_name
+            )
+        )
+        db_project_id = db_project_id.scalars().first()
+    return db_project_id
