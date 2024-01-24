d53bc8de1da40d55825def112e7c7edfc3bea277
aten88 <virtuoso_ten@mail.ru>
2024-01-24 16:05:49 +0300

    Написал Pydantic-schema для ответа на запросы из БД

--- app/api/charity_project.py
+++ app/api/charity_project.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, HTTPException
 
 from app.crud.charity_project import create_charity_project, get_project_id_by_name
-from app.schemas.charity_project import CharityProjectCreate
+from app.schemas.charity_project import CharityProjectCreate, CharityProjectDB
 
 router = APIRouter()
 
 
-@router.post('/charity_project/')
+@router.post(
+    '/charity_project/',
+    response_model=CharityProjectDB,
+    response_model_exclude_none=True,
+    response_model_exclude_defaults=True,
+)
 async def create_new_charity_project(
     charity_project: CharityProjectCreate,
 ):
--- app/schemas/charity_project.py
+++ app/schemas/charity_project.py
@@ -6,7 +6,23 @@ from pydantic import BaseModel, Field, validator
 from app.core.constants import MIN_LEN_FIELD, MAX_LEN_FIELD
 
 
-class CharityProjectCreate(BaseModel):
+class CharityProjectBase(BaseModel):
+    name: str = Field(
+        None,
+        min_length=MIN_LEN_FIELD,
+        max_length=MAX_LEN_FIELD
+    )
+    description: str = Field(None, min_length=MIN_LEN_FIELD)
+    full_amount: int
+
+    @validator('full_amount')
+    def check_full_amount(cls, value):
+        if value <= 0:
+            raise ValueError('Поле full_amount должно быть больше 0')
+        return value
+
+
+class CharityProjectCreate(CharityProjectBase):
     """ Схема для создания обьекта CharityProject. """
     name: str = Field(
         ...,
@@ -14,14 +30,15 @@ class CharityProjectCreate(BaseModel):
         max_length=MAX_LEN_FIELD
     )
     description: str = Field(..., min_length=MIN_LEN_FIELD)
-    full_amount: int
+
+
+class CharityProjectDB(CharityProjectBase):
+    """Схема получения данных из БД. """
+    id: int
     invested_amount: Optional[int]
     fully_invested: Optional[bool]
     create_date: Optional[datetime]
     close_date: Optional[datetime]
 
-    @validator('full_amount')
-    def check_full_amount(cls, value):
-        if value <= 0:
-            raise ValueError('Поле full_amount должно быть больше 0')
-        return value
+    class Config:
+        orm_mode = True
